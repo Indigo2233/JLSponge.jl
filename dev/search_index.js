42cf79f279db5743b618ba79f091a63662b8b65e
Documenter.jl <documenter@juliadocs.github.io>
2021-05-06 14:27:09 +0000

    build based on b014559

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JLSponge","category":"page"},{"location":"#JLSponge","page":"Home","title":"JLSponge","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JLSponge]","category":"page"}]
+}
