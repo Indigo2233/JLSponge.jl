3f0c69a1837c84e50deef430f0b4f4877fbf2a31
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-06 20:50:48 +0800

    remake docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(JLSponge, :DocTestSetup, :(using JLSponge); recursive=true)
 makedocs(;
     modules=[JLSponge],
     authors="suyaoIndigo <j.c.f.gauss@sjtu.edu.cn> and contributors",
-    repo="https://github.com/sjtu_suyao/JLSponge.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/AquaIndigo/JLSponge.jl/blob/{commit}{path}#{line}",
     sitename="JLSponge.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://sjtu_suyao.github.io/JLSponge.jl",
+        canonical="https://aquaindigo.github.io/JLSponge.jl",
         assets=String[],
     ),
     pages=[
