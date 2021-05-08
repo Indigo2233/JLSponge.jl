0588b6c23d8be292c7cac2e87c9a383e42db9eb6
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-08 14:08:10 +0800

    test for fsm_active_close

--- test/fsm_active_close.jl
+++ test/fsm_active_close.jl
@@ -27,21 +27,19 @@
         expect_state(conn, JLSponge.CLOSED)
     end
     
-    # @testset "start in FIN_WAIT_2, send FIN, time out" begin
-    #     conn = fsm_in_fin_wait_2()
-    #     tick!(conn, 4TIMEOUT_DFLT)
-    #     expect_state(conn, JLSponge.FIN_WAIT_2)
+    @testset "start in FIN_WAIT_2, send FIN, time out" begin
+        conn = fsm_in_fin_wait_2()
+        tick!(conn, 4TIMEOUT_DFLT)
+        expect_state(conn, JLSponge.FIN_WAIT_2)
 
-    #     rx_seqno = WrappingInt32(1)
-    #     send_fin!(conn, rx_seqno, WrappingInt32(2))
+        rx_seqno = WrappingInt32(1)
+        send_fin!(conn, rx_seqno, WrappingInt32(2))
 
-    #     send_ack!(conn, WrappingInt32(2), WrappingInt32(2))
-    #     expect_no_seg(conn)
-    #     expect_state(conn, JLSponge.TIME_WAIT)
-        
-    #     tick!(conn, 10TIMEOUT_DFLT - 1)
-    #     expect_state(conn, JLSponge.TIME_WAIT)
-    #     tick!(conn, 2)
-    #     expect_state(conn, JLSponge.CLOSED)
-    # end
+        tick!(conn, 1)
+        expect_one_seg(conn; ack=true, ackno=rx_seqno + 1)
+        expect_state(conn, JLSponge.TIME_WAIT)
+
+        tick!(conn, 10TIMEOUT_DFLT)
+        expect_state(conn, JLSponge.CLOSED)
+    end
 end
