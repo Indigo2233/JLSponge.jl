dbf227e02d1c6ef88b295d8c8f2e188f27cd5644
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-08 22:06:54 +0800

    tests for fsm_passive_close

--- test/fsm_passive_close.jl
+++ test/fsm_passive_close.jl
@@ -36,5 +36,33 @@
         expect_state(conn, JLSponge.CLOSED)
     end
    
-    
+    @testset "start in ESTABLISHED, send FIN, recv ACK, check for CLOSE_WAIT" begin
+        conn = fsm_in_established()
+        tick!(conn, 4TIMEOUT_DFLT)
+        expect_state(conn, JLSponge.ESTABLISHED)
+
+        rx_seqno = WrappingInt32(1)
+        ack_expect = rx_seqno + 1
+        send_fin!(conn, rx_seqno, WrappingInt32(0))
+        tick!(conn, 1)
+
+        expect_one_seg(conn, ack=true, ackno=ack_expect)
+        expect_state(conn, JLSponge.CLOSE_WAIT)
+
+        send_fin!(conn, rx_seqno, WrappingInt32(0))
+        tick!(conn, 1)
+
+        expect_one_seg(conn; ack=true, ackno=ack_expect)
+
+        expect_state(conn, JLSponge.CLOSE_WAIT)
+
+        tick!(conn, 1)
+        end_input_stream!(conn)
+        tick!(conn, 1)
+        expect_state(conn, JLSponge.LAST_ACK)
+        seg3 = expect_one_seg(conn; fin=true)
+        send_ack!(conn, ack_expect, seg3.header.seqno + 1)        
+        tick!(conn, 1)
+        expect_state(conn, JLSponge.CLOSED)
+    end
 end
