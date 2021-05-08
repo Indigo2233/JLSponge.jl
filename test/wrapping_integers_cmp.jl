44e95b2045111f49c8b48c16375d44c17982708f
AquaIndigo <j.c.f.gauss@sjtu.edu.cn>
2021-05-08 14:02:35 +0800

    test for wrapping_integers_cmp

--- test/wrapping_integers_cmp.jl
+++ test/wrapping_integers_cmp.jl
@@ -4,10 +4,10 @@
         N_REPS = 4096
         for i in 1:N_REPS
             n = rand(typemin(UInt32):typemax(UInt32))
-            diff = rand(typemin(UInt16):typemax(UInt16))
+            diff = rand(UInt16(1):typemax(UInt16))
             m = n + diff
             @test WrappingInt32(m) != WrappingInt32(n)
-        end 
+        end
     end    
 end
 
